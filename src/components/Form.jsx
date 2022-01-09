3b6e09e3fc3b031a8ef1f22b50c947e224fb5c99
Gregorio Moreta <gregoriomoreta4@gmail.com>
2022-01-08 19:27:29 -0500

    have everything for authentication except for rendering the information from the api call/ backend schema

---
+++ src/components/Form.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { useAppState } from "../AppState";
+
+const Form = (props) => {
+
+    const { state, dispatch } = useAppState()
+    const {token} = state
+    const action = props.match.params.action
+    const [formData, setFormData] = React.useState(state[action])
+
+    const actions = {
+        new: () => {
+            return fetch(state.url + "/posts", {
+                method: "post",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: "bearer " + token
+                },
+                body: JSON.stringify(formData),
+            }).then((response) => response.json())
+        },
+        edit: () => {
+            return fetch(state.url + "/posts/" + state.edit.id, {
+                method: "put",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: "bearer " + token
+                },
+                body: JSON.stringify(formData),
+            }).then((response) => response.json())
+        }
+    }
+
+    const handleChange = (event) => {
+        setFormData({...formData, [event.target.name]: event.target.value})
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        actions[action]().then((data) => {
+            props.getPosts()
+            props.history.push("/dashboard/")
+        })
+    }
+
+    return (
+        <div className="form">
+            <form onSubmit={handleSubmit}>
+                <input 
+                    type="text" 
+                    name="title" 
+                    value={formData.title} 
+                    onChange={handleChange}>
+                </input>
+                <input 
+                    type="text" 
+                    name="author" 
+                    value={formData.author} 
+                    onChange={handleChange}>
+
+                </input>
+                <input 
+                    type="text" 
+                    name="body" 
+                    value={formData.body} 
+                    onChange={handleChange}>
+                </input>
+                <input 
+                    type="submit"  
+                    value={action}>
+                </input>
+            </form>
+        </div>
+    )
+};
+
+export default Form;
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,19 +1,34 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import { useAppState } from "../AppState.jsx";
 
 const Nav = (props) => {
+
+    const { state, dispatch } = useAppState()
+
     return <header>
         <h1>Welcome to Bloggy!</h1>
         <nav>
             <Link to="/">
                 <div>Home</div>
             </Link>
+            {!state.token ? (
+            <>
             <Link to="/auth/signup">
                 <div>Signup</div>
             </Link>
             <Link to="/auth/login">
                 <div>Login</div>
             </Link>
+            </>) : null}
+            {state.token ? <div 
+                onClick={() => {
+                dispatch({type: "logout"})
+                props.history.push("/")
+            }}
+            >
+            Logout
+            </div> : null}
         </nav>
     </header>
 };
